966911d71a6a51b51aa78d7ccd356d2ad342bca4
Sheena <Sheena>
2020-04-08 14:16:53 +0530

    modified the message

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is a sample hello world application");
+		System.out.println("This is a sample hello world application to test jenkins build");
 
 	}
 
